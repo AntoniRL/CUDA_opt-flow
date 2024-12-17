179f59d249f0a8413ce76d059b47fbfe2d8e07d8
AntoniRL <aradominski@student.agh.edu.pl>
2024-12-17 12:17:53 +0100

    init files

---
+++ src/kernels.cu
@@ -0,0 +1,12 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+__global__ void optFlow_LucasKanade(float *N1, float *N2, float *P, const int height, const int width)
+{
+    //@@ INSERT CODE HERE
+}
+
+__global__ void optFlow_HornSchunck(float *N1, float *N2, float *P, const int height, const int width)
+{
+    //@@ INSERT CODE HERE
+}
